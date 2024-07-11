1a508219346c74630711516f3ba2b2268b36461e
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-07-11 16:46:33 -0400

    rename

--- src/openaiClient.jsx
+++ src/openaiClient.jsx
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-const openaiKey = import.meta.env.VITE_PUBLIC_OPENAI_KEY;
+const openaiKey = import.meta.env.VITE_PUBLIC_OPENAI;
 
 export const openai = new OpenAI({
   apiKey: openaiKey,

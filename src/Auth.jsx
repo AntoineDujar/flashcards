40b29bfe56b970343f26efd19f82fb495ec66633
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-11-13 11:49:05 +0800

    added username password login

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -14,28 +14,40 @@ import {
   Text,
   useBoolean,
   Input,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 
 export default function Auth() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
 
   const handleEmailChange = (event) => {
     if (event) {
       setEmail(event.target.value);
     }
   };
+  const handlePasswordChange = (event) => {
+    if (event) {
+      setPassword(event.target.value);
+    }
+  };
 
   const handleLogin = async () => {
     setLoading(true);
-    const { error } = await supabase.auth.signInWithOtp({ email });
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
 
-    if (error) {
-      alert(error.error_description || error.message);
-    } else {
-      alert("Check your email for the login link!");
-      window.close();
-    }
+    if (error) alert(error.error_description || error.message);
+    if (!session) alert("Check your email for the login link!");
     setLoading(false);
   };
 
@@ -43,16 +55,29 @@ export default function Auth() {
     <>
       <Card>
         <CardHeader>
-          <Heading>Login with Magic Links</Heading>
+          <Heading>Log in</Heading>
         </CardHeader>
         <CardBody>
           <Input
-            placeholder="Your Email"
+            placeholder="email@email.com"
             onChange={handleEmailChange}
             value={email}
           />
+          <InputGroup size="md">
+            <Input
+              placeholder="Password"
+              onChange={handlePasswordChange}
+              type={show ? "text" : "password"}
+              value={password}
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {show ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
           <Button colorScheme="blue" size="lg" onClick={() => handleLogin()}>
-            {loading ? <span>Loading</span> : <span>Send magic link</span>}
+            {loading ? <span>Loading</span> : <span>Log in</span>}
           </Button>
         </CardBody>
       </Card>
